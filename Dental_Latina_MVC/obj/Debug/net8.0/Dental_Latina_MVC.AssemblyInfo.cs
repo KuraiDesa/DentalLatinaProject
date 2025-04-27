831d9e489d6547095437915170910add6c013e0f
KuraiDesa <rafarivro4to3@gmail.com>
2025-04-26 23:17:49 -0300

    Rama nueva (#4)
    * Soto y la que lo pario
    
    * Hola
    
    * ta
    
    * Delete .vs directory
    
    * POR FAVOR BASTA QUIERO COMER
    
    ---------
    
    Co-authored-by: Rafa <dr269299@fi365.ort.edu.uy>

--- Dental_Latina_MVC/obj/Debug/net8.0/Dental_Latina_MVC.AssemblyInfo.cs
+++ Dental_Latina_MVC/obj/Debug/net8.0/Dental_Latina_MVC.AssemblyInfo.cs
@@ -14,7 +14,15 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dental_Latina_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
+<<<<<<< HEAD
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9db81d51538894d1ec566da51de2fec6095946d")]
+=======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1804176b54023fac37bb7f1a67f6bd8e01b5c96")]
+>>>>>>> 79c06a3ae3ed2816a87fc4887da38bed112fd919
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1804176b54023fac37bb7f1a67f6bd8e01b5c96")]
+>>>>>>> 79c06a3ae3ed2816a87fc4887da38bed112fd919
 [assembly: System.Reflection.AssemblyProductAttribute("Dental_Latina_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dental_Latina_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
