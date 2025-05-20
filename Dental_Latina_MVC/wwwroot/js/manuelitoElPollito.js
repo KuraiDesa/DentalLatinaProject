506953457885080dd8d834e2c666fc811beedc33
Rafa <dr269299@fi365.ort.edu.uy>
2025-05-20 13:43:01 -0300

    Se agrego la funcionalidad de busqueda en gestionar y eliminar productos

--- Dental_Latina_MVC/wwwroot/js/manuelitoElPollito.js
+++ Dental_Latina_MVC/wwwroot/js/manuelitoElPollito.js
@@ -1,4 +1,77 @@
-﻿$(function () {
+﻿function filtrar(seccion, categoria, nombre) {
+    let categoriaId = document.getElementById(categoria);
+    let nombreProd = document.getElementById(nombre);
+    let seccionProd = document.getElementById(seccion); 
+    let nombreval = nombreProd.value;
+    let categoriaval = categoriaId.value;
+    $.ajax({
+        url: '/Admin/FilterByCategoria',
+        method: 'GET',
+        data: {
+            categoria: categoriaval,
+            nombre: nombreval
+        },
+        dataType: 'json',
+        success: function (data) {
+            seccionProd.innerHTML = "";
+            if (seccion == 'resultadosBusquedaEliminar') {
+                data.forEach(prod => {
+                    let precio;
+                    if (prod.precio != 0) {
+                        precio = `<p class= "card-text"> $${prod.precio}</p>`
+                    } else {
+                        precio = '<p class="card-text" style="height: 24px;"> </p>'
+                    }
+                    seccionProd.innerHTML += `
+                <div class="col-md-4 mb-4 d-flex">
+                    <div class="card product-card flex-fill">
+                            <img src="${prod.photoUrl}" class="card-img-top product-card-img" alt="${prod.nombre}">
+                            <div class="card-body">
+                                    <h5 class="card-title text-center">${prod.nombre}</h5>
+                                    ${precio}
+                                <div class="text-center">
+                                        <a href="#" class="btn btn-danger col-10" data-bs-toggle="modal" data-bs-target="#confirmarEliminarModal" data-producto-id="${prod.id}">Eliminar</a>
+                                </div>
+                            </div>
+                    </div>
+                </div >
+                `
+                });
+            } else {
+                data.forEach(prod => {
+                    let precio;
+                    if (prod.precio != 0) {
+                        precio =`<p class= "card-text"> $${ prod.precio }</p>`
+                    } else {
+                        precio = '<p class="card-text" style="height: 24px;"> </p>'
+                    }
+                    seccionProd.innerHTML += `
+                <div class="col-md-4 mb-4 d-flex">
+                    <div class="card product-card flex-fill">
+                            <img src="${prod.photoUrl}" class="card-img-top product-card-img" alt="${prod.nombre}">
+                            <div class="card-body">
+                                    <h5 class="card-title text-center">${prod.nombre}</h5>
+                                    ${precio}
+                                <div class="text-center">
+                                        <a href="#" class="btn btn-warning col-10">Modificar</a>
+                                </div>
+                            </div>
+                    </div>
+                </div >
+                `
+                });
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error('AJAX error:', status, error);
+            $('#subcategoriaProducto')
+                .html('<option value="">Error cargando subcategorías</option>');
+        }
+    });
+}
+
+
+$(function () {
     $('#categoriaProducto').on('change', function () {
         var categoriaId = $(this).val();
         if (!categoriaId) {
